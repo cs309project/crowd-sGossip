2c665e2875f833d560011573661924788afd7f0d
yousef alaa <67870537+pegimon@users.noreply.github.com>
2022-12-15 21:24:28 +0200

    Update Register.jsx

--- crowds-gossip-frontend/src/components/register/Register.jsx
+++ crowds-gossip-frontend/src/components/register/Register.jsx
@@ -5,9 +5,6 @@ import React,{useState }from "react";
 import styled from 'styled-components';
 import Logo from "../../assets/logo2.png";
 
-// import {ToastContainer} from"react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import axios from "axios";
 
 
  function Register() {
@@ -23,67 +20,7 @@ import Logo from "../../assets/logo2.png";
   
    }  );
   //btshtghl lma el A ttghiar
-  
-  
-  // const toastOptions = {
-  //   position: "bottom-right",
-  //   autoClose: 8000,
-  //   pauseOnHover: true,
-  //   draggable: true,
-  //   theme: "dark",
-  // };
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   if (handleValidation()) {
-  //     //btakhod el values bta3tha mn el values
-  //     const { password , username ,email  } = values;
-  //     const { data } = await axios.post( {
-  //       username,
-  //       email,
-  //       password,
-  //     });
-
-  //     if (data.status === false) {
-  //       toast.error(data.msg, toastOptions);
-  //     }
-  //     if (data.status === true) {
-  //       localStorage.setItem('chat-app-user', JSON.stringify(data.user));
-    
-  //     }
-  //   }
-  // };
-  
-  // const handleValidation = () => {
-  //   const { password, confirmPassword, username, email } = values;
-  //   if (password !== confirmPassword) {
-  //     toast.error(
-  //       "Password and confirm password should be same.",
-  //       toastOptions
-  //     );
-  //     return false;
-  //   } else if (username.length < 3) {
-  //     toast.error(
-  //       "Username should be greater than 3 characters.",
-  //       toastOptions
-  //     );
-  //     return false;
-  //   } else if (password.length < 8) {
-  //     toast.error(
-  //       "Password should be equal or greater than 8 characters.",
-  //       toastOptions
-  //     );
-  //     return false;
-  //   } else if (email === "") {
-  //     toast.error("Email is required.", toastOptions);
-  //     return false;
-  //   }
-
-  //   return true;
-  // };
-
- 
-
-  
+   
 
   const handleChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.value });
