0b37f3a18c1317df2d74675e23dbcc2d4358a56a
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-12-17 22:44:29 +0200

    removed posts dummy data

--- crowds-gossip-frontend/src/components/feed component/Feed.js
+++ crowds-gossip-frontend/src/components/feed component/Feed.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Post from '../post component/Post'
 
 function Feed() {
-    const [posts, setPosts] = useState(dummyPosts)
+    const [posts, setPosts] = useState([])
 
     return (
         <div className='feed-container'>
@@ -18,41 +18,5 @@ function Feed() {
         </div>
     )
 }
-
-
-//this is a dummy data to test how the feed will look like
-const dummyPosts = [
-    {
-        profileIcon: 'https://4.bp.blogspot.com/-klQjV5L27_s/T73fozhXluI/AAAAAAAAAkI/_75K0D7Hpqw/s1600/smiley-face2.jpg',
-        authorName: 'Test User 1',
-        text: 'This is a test post',
-        comments: [
-            {
-                username: 'Youssef 3ala2',
-                text: 'Hello people'
-            },
-            {
-                username: 'Youssef Salah',
-                text: 'Hello humans'
-            },
-            {
-                username: 'Youssef Salah',
-                text: 'Hello humans'
-            }
-        ]
-    },
-    {
-        profileIcon: 'https://4.bp.blogspot.com/-klQjV5L27_s/T73fozhXluI/AAAAAAAAAkI/_75K0D7Hpqw/s1600/smiley-face2.jpg',
-        authorName: 'Test User 2',
-        text: 'Posting dummy data',
-        comments: []
-    },
-    {
-        profileIcon: 'https://4.bp.blogspot.com/-klQjV5L27_s/T73fozhXluI/AAAAAAAAAkI/_75K0D7Hpqw/s1600/smiley-face2.jpg',
-        authorName: 'Test User 3',
-        text: 'Rumors...... 😁',
-        comments: []
-    }
-]
 
 export default Feed
