fde592e4a387da8140bfa370ca00deb146d1cf68
Jhany01 <johnhany07@gmail.com>
2022-12-22 22:19:06 +0200

    Enhanced styling of Logo

--- crowds-gossip-frontend/src/components/logo-component/Logo.js
+++ crowds-gossip-frontend/src/components/logo-component/Logo.js
@@ -4,10 +4,8 @@ import logo from './Logo.png'
 
 function Logo() {
   return (
-    <div className='logo'>
       <img className='image' src={logo}
         alt='No image found' />
-    </div>
   )
 }
 
