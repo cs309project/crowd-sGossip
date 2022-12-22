4c0df4f1c308b0138fe323f4c8fe60d8e4a267b1
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-12-22 21:16:47 +0200

    Enhanced voting component styling

--- crowds-gossip-frontend/src/components/voting component/Voting.js
+++ crowds-gossip-frontend/src/components/voting component/Voting.js
@@ -3,28 +3,26 @@ import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import './Voting.css'
 
-function Voting({upVoters, downVoters, _id}) {
+function Voting({ upVoters, downVoters, _id }) {
     const [upVotes, setUpVotes] = useState(upVoters.length)
     const [downVotes, setDownVotes] = useState(downVoters.length)
 
     return (
         <div className='vote-container'>
-            <div className='arrow-up-div'>
+            <div className='up-vote-container'>
                 <ArrowDropUpIcon
+                    className='arrow-up'
                     fontSize='large'
                     onClick={() => alert('up vote')}
                 />
-            </div>
-            <div className='up-vote-stat'>
                 <p>{upVotes}</p>
             </div>
-            <div className='down-vote-stat'>
+            <div className='down-vote-container'>
                 <p>{downVotes}</p>
-            </div>
-            <div className='arrow-down-div'>
                 <ArrowDropDownIcon
-                fontSize='large'
-                onClick={() => alert('down vote')}
+                    className='arrow-down'
+                    fontSize='large'
+                    onClick={() => alert('down vote')}
                 />
             </div>
         </div>
