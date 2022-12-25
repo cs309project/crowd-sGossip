d1db1555cb0779c87e99fdcb9f74e982b003a852
yousef alaa <yousefalaa2279@gmail.com>
2022-12-25 11:46:07 +0200

    unused function

--- crowds-gossip-frontend/src/App.js
+++ crowds-gossip-frontend/src/App.js
@@ -3,12 +3,7 @@ import Login from "./pages/login/Login";
 import {Routes,Route} from "react-router-dom";
 import Register from "./pages/register/Register";
 import Feed from "./pages/home/Feed";
-import * as API from './API/User'
 function App() {
-  const requestChange = async ()=>{
-    const users = await API.userSearch("y")
-    console.log(users)
-  }
   return (
     <Routes>
         <Route exact path={'/'} element={<Feed name={'name'} followers={[{name:"mae,"}]} followings={[{name:"dd"}]} posts={[]}/>}/>
