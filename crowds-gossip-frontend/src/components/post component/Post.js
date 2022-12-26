8bc043b69523761bd10e9372090a54337fbe9e20
Jhany01 <johnhany07@gmail.com>
2022-12-26 17:20:05 +0200

    -rendering retrieved posts -Add comment functionality

--- crowds-gossip-frontend/src/components/post component/Post.js
+++ crowds-gossip-frontend/src/components/post component/Post.js
@@ -1,15 +1,53 @@
 import './Post.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Comment from '../comment-component/Comment'
 import Voting from '../voting component/Voting';
-function Post({ profileIcon, authorName, text, photoLink, comments }) {
+import { addComment, getPostsById } from '../../API/Post.js';
+import { getById, } from '../../API/User';
+
+function Post({currentUser, postID }) {
+
     const [commentText, setCommentText] = useState('')
+    const [post, setPost] = useState("")
+    const [authorName, setAuthorName] = useState("")
+    const [text, setText] = useState("")
+    const [comments, setComments] = useState([])
+    const [profileIcon, setProfileIcon] = useState('')
+    const [photoLink, setPhotoLink] = useState('')
+    const [updated, setUpdated] = useState(false)
+
+
     const change = (e) => {
         setCommentText(e.target.value)
     }
-    const addComment = () => {
-
+    const handleAddComment = async () => {
+       await addComment({_id:postID, commenter:currentUser._id.toString(), comment: commentText})
+       setUpdated(!updated)
     }
+
+    useEffect(() => {
+        if (postID) {
+            async function getPost() {
+                setPost(await getPostsById({ _id: postID.toString() }))
+            }
+            getPost()
+        }
+    }, [postID , updated])
+
+    useEffect(() => {
+        if (post) {
+            async function setPostData() {
+                setText(post.content)
+                setComments(post.comments)
+                await getById(post.author.toString()).then(e => {
+                    setAuthorName(e.name)
+                })
+            }
+            setPostData()
+        }
+    }, [post])
+
+
     return (
         <div className={'container'}>
             <div className='div-to-flex'>
@@ -36,15 +74,17 @@ function Post({ profileIcon, authorName, text, photoLink, comments }) {
                 <div style={{ display: 'flex' }}>
                     <img src={profileIcon} alt={""} />
                     <input placeholder={'write a comment'} onChange={change} />
-                    <button className={'comment-button'} onClick={addComment}>comment</button>
+                    <button className={'comment-button'} onClick={handleAddComment}>comment</button>
                 </div>
-                {comments.map((value, index) => {
-                    return <Comment
-                        text={value.text}
-                        userName={value.userName}
-                        profilePic={value.profilePic}
-                        key={index} />
-                })}
+                {
+                    comments && comments.length ? comments.map((value, index) => {
+                        return <Comment
+                            text={value.comment}
+                            commenterId={value.commenter}
+                            profilePic={value.profilePic}
+                            key={index} />
+                    }) : null
+                }
             </div>
         </div>
     );
