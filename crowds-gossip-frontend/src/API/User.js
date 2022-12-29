f3b9f30706cf5ca6c81994b07191e74ad7581d5d
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-12-29 17:07:29 +0200

    added unban function

--- crowds-gossip-frontend/src/API/User.js
+++ crowds-gossip-frontend/src/API/User.js
@@ -6,49 +6,53 @@ const cookie = new Cookies()
 const token = cookie.get('Authorization')
 
 
-export const getUsers = async ()=> {
-    return await (await axios.get('http://localhost:8000/users',{ headers: { 'Authorization': token } })).data
+export const getUsers = async () => {
+    return await (await axios.get('http://localhost:8000/users', { headers: { 'Authorization': token } })).data
 }
-export const getById = async (id) => {return await (await axios.get(`${proxy}users/user/${id}`,{ headers: { 'Authorization': token } })).data }
+export const getById = async (id) => { return await (await axios.get(`${proxy}users/user/${id}`, { headers: { 'Authorization': token } })).data }
 
-export const register = async (user)=>{
-    const {name,email,password}=user
-    return await axios.post('http://localhost:8000/users/register',{
-        name:name,
-        email:email,
-        password:password      
+export const register = async (user) => {
+    const { name, email, password } = user
+    return await axios.post('http://localhost:8000/users/register', {
+        name: name,
+        email: email,
+        password: password
     })
 }
-export const login = async(user)=>{
-    const {email,password} = user
-    return await axios.post('http://localhost:8000/users/login',{
-        email:email,
-        password:password
-    } )
+export const login = async (user) => {
+    const { email, password } = user
+    return await axios.post('http://localhost:8000/users/login', {
+        email: email,
+        password: password
+    })
 }
 
-export const userSearch = async (sname)=>{
-    return await (await axios.get(`${proxy}users/userSearch?sname=${sname}`,{ headers: { 'Authorization': token } })).data
+export const userSearch = async (sname) => {
+    return await (await axios.get(`${proxy}users/userSearch?sname=${sname}`, { headers: { 'Authorization': token } })).data
 }
 
-export const addImage = async (photo)=>{
-    return await (await axios.post(`${proxy}users/addImage`,{photo:photo},{ headers: { 'Authorization': token } })).data
+export const addImage = async (photo) => {
+    return await (await axios.post(`${proxy}users/addImage`, { photo: photo }, { headers: { 'Authorization': token } })).data
 }
 
-export const updateUser = async (name,photo)=>{
-    return await (await axios.post(`${proxy}users/update`,{photo:photo,name:name},{ headers: { 'Authorization': token } })).data
+export const updateUser = async (name, photo) => {
+    return await (await axios.post(`${proxy}users/update`, { photo: photo, name: name }, { headers: { 'Authorization': token } })).data
 }
 
-export const followUser = async (uid)=>{
+export const followUser = async (uid) => {
     console.log(uid)
-    return await (await axios.post(`${proxy}users/followUser`,{idToFollow:uid},{headers:{'Authorization':token}})).data
+    return await (await axios.post(`${proxy}users/followUser`, { idToFollow: uid }, { headers: { 'Authorization': token } })).data
 }
 
-export const unfollowUser = async (uid)=>{
+export const unfollowUser = async (uid) => {
     console.log(uid)
-    return await (await axios.post(`${proxy}users/unfollowUser`,{idToUnfollow:uid},{headers:{'Authorization':token}})).data
+    return await (await axios.post(`${proxy}users/unfollowUser`, { idToUnfollow: uid }, { headers: { 'Authorization': token } })).data
 }
 
-export const banUser = async (uid) =>{
-    await axios.post(`${proxy}users/block`,{id:uid} , {headers:{'Authorization':token}})
+export const banUser = async (uid) => {
+    await axios.post(`${proxy}users/block`, { id: uid }, { headers: { 'Authorization': token } })
+}
+
+export const unbanUser = async (uid) => {
+    await axios.post(`${proxy}users/unblock`, { id: uid }, { headers: { 'Authorization': token } })
 }
