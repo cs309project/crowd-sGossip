b50c54f908f4c2a03db2c28d352638877d38c68f
yousef alaa <yousefalaa2279@gmail.com>
2022-12-29 03:12:11 +0200

    edit profile

---
+++ crowds-gossip-frontend/src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    publicKey:"public_X7yTliORSXHPEjwCoN+adNnMArw=",
+    urlEndpoint:'https://ik.imagekit.io/CrowdsGossip1/',
+    authEndpoint:'http://localhost:8000/auth'
+}
+
+export default config
--- crowds-gossip-frontend/src/pages/edit profile/EditProfile.js
+++ crowds-gossip-frontend/src/pages/edit profile/EditProfile.js
@@ -3,13 +3,25 @@ import Header from '../../components/header-component/Header';
 import './EditProfile.css'
 import { useNavigate } from 'react-router-dom';
 import * as API from '../../API/User'
+import { IKUpload, IKContext } from 'imagekitio-react';
+import config from '../../config'
 
 function EditProfile() {
     const navigate = useNavigate()
-
-    const [name, setName] = useState('')
-    const [photo , setPhoto] = useState('')
+    const [name, setName] = useState(undefined)
+    const [photo , setPhoto] = useState(undefined)
     const [user , setUser] = useState({})
+
+    const onError = err => {
+        console.log("Error", err);
+    };
+      
+    const onSuccess = res => {
+        setPhoto(config.urlEndpoint+'/users'+res.filePath)
+    };
+   const handleClick = ()=>{
+        document.getElementById('upload').click()
+   }
     useEffect(() => {
         const getUser = async ()=>{
             setUser(await API.getById())
@@ -19,12 +31,28 @@ function EditProfile() {
     }, []);
 
     const handleSave = async () => {
+        if (!photo)setPhoto(user.photo)
+        if (!name)setName(user.name)
         await API.updateUser(name,photo)
+        navigate('/profile')
     }
     return (
         <div className='edit-container'>
             <Header />
-            <img src={user.photo} alt={''}/>
+            <h2>press image to upload new image to your profile</h2>
+            <img src={photo||user.photo} alt={""} onClick={handleClick}/>
+            <IKContext 
+            publicKey={config.publicKey} 
+            urlEndpoint={config.urlEndpoint+'/users'}
+            authenticationEndpoint={config.authEndpoint} 
+            >
+                <IKUpload
+                    onError={onError}
+                    onSuccess={onSuccess}
+                    id={'upload'}
+                    style={{display:'none'}}
+                />
+            </IKContext>
             <input
                 placeholder='Change your user name'
                 type={'text'}
